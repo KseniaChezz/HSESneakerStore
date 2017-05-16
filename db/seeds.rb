1e850e866e6f7bab29d278c841e43e37af34fc6f
KseniaChezz <ksushaachesnokova@gmail.com>
2017-05-16 12:10:27 +0300

    added Images and Logos

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,58 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Brand.create([
-  {
-    name: 'Nike'
-  }, {
-    name: 'Adidas'
-  }, {
-    name: 'New Balance'
-  }, {
-    name: 'Reebok'
-  }, {
-    name: 'Puma'
-  }
-])
+Rake::Task['db:drop'].invoke
+Rake::Task['db:create'].invoke
+Rake::Task['db:migrate'].invoke
+
+def random_brand_id
+  Brand.offset(rand(Brand.count)).first.id
+end
+
+def upload_fake_image
+  uploader = ImageUploader.new(Sneaker.new, :image)
+  uploader.cache!(File.open(Dir.glob(File.join(Rails.root, 'lib/tasks/sneakers', '*')).sample))
+  uploader
+end
+
+def upload_fake_logo
+  uploader = LogoUploader.new(Logo.new, :logo)
+  uploader.cache!(File.open(Dir.glob(File.join(Rails.root, 'lib/tasks/brands', '*')).sample))
+  uploader
+end
+
+@sneaker_models = ['Air Max', 'Cortez', 'Blazer', 'Dunk', 'Zoom']
+@brand_names = ['Nike', 'Adidas', 'New Balance', 'Reebok', 'Puma', 'Yonex', 'Merrell', 'Columbia']
+@sneaker_sex = ['male', 'female', 'unisex']
+
+
+def sneaker_price
+  rand(8000..25000)
+end
+
+def create_sneaker
+  Sneaker.create(
+    brand_id: random_brand_id,
+    model: @sneaker_models.sample,
+    sex: @sneaker_sex.sample,
+    price: sneaker_price,
+    image: upload_fake_image
+  )
+end
+
+def create_brand
+  Brand.create(
+    logo: upload_fake_logo,
+    name: @brand_names.sample
+  )
+end
+
+8.times do
+  brand = create_brand
+  puts "Brand #{brand.id} created"
+end
+
+75.times do
+  sneaker = create_sneaker
+  puts "Sneaker #{sneaker.id} created"
+end
